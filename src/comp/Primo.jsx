1af3accae8cb575135854871076fefe56d9cabc1
marisolkarina <marisol.kari.20@gmail.com>
2025-07-05 21:57:27 -0500

    solo manual, manual y automatico - numeros primos

--- src/comp/Primo.jsx
+++ src/comp/Primo.jsx
@@ -4,7 +4,7 @@ class Primo extends Component {
 
     render() {
 
-        const {soloManual, siguientePrimo, anteriorPrimo, numeroPrimo} = this.props;
+        const {manual, siguientePrimo, anteriorPrimo, numeroPrimo} = this.props;
 
         return (
             <main className="card my-2 mx-auto col-4">
@@ -12,7 +12,7 @@ class Primo extends Component {
                     <p className="lead text-primary text-center fw-medium">Número primo: {numeroPrimo}</p>
                 </div>
                 {
-                    soloManual ? (
+                    manual ? (
                         <div className="card-body d-flex justify-content-center">
                             <button className="btn btn-success mb-3 mx-2" onClick={()=>anteriorPrimo()}>Anterior</button>
                             <button className="btn btn-primary mb-3 mx-2" onClick={()=>siguientePrimo()}>Siguiente</button>
--- src/comp/VisorPrimo.jsx
+++ src/comp/VisorPrimo.jsx
@@ -4,12 +4,13 @@ import Primo from "./Primo";
 class VisorPrimo extends Component {
 
     state = {
-        numeroPrimo: 2,
-        numeroPrimoAuto: 2
+        numeroPrimoM: 2, // manual
+        numeroPrimoA: 2, // automatico
+        auto: true // la doble funcionalidad (manual - auto) inicia en automatico(auto: true) y puede volverse manual (auto: false)
     }
 
     esPrimo = (numero) => {
-        for (let i=2; i<=Math.sqrt(numero); i++) {
+        for (let i = 2; i <= Math.sqrt(numero); i++) {
             if (numero % i == 0) return false;
         }
         return true;
@@ -17,7 +18,7 @@ class VisorPrimo extends Component {
 
     buscarPrimo = (numero, avanzaRetrocede) => {
         let num = numero + avanzaRetrocede;
-        while(num>1 && !this.esPrimo(num)) {
+        while (num > 1 && !this.esPrimo(num)) {
             num += avanzaRetrocede;
         }
         return num < 2 ? 2 : num;
@@ -25,40 +26,74 @@ class VisorPrimo extends Component {
 
     siguientePrimo = () => {
         this.setState({
-            numeroPrimo: this.buscarPrimo(this.state.numeroPrimo, 1)
+            numeroPrimoM: this.buscarPrimo(this.state.numeroPrimoM, 1)
         })
     }
 
     anteriorPrimo = () => {
         this.setState({
-            numeroPrimo: this.buscarPrimo(this.state.numeroPrimo, -1)
+            numeroPrimoM: this.buscarPrimo(this.state.numeroPrimoM, -1)
         })
     }
 
     actualizarPrimo = () => {
         this.setState({
-            numeroPrimoAuto: this.buscarPrimo(this.state.numeroPrimoAuto, 1)
+            numeroPrimoA: this.buscarPrimo(this.state.numeroPrimoA, 1)
         })
     }
 
-    componentDidMount(){
-        this.id = setInterval(() => {
-            this.actualizarPrimo()
-        }, 2000);
+    componentDidMount() {
+         // solo cambia cada 2 seg cuando es automatico
+            this.id = setInterval(() => {
+                if (this.state.auto) this.actualizarPrimo()
+            }, 2000);
+
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this.id)
     }
 
-    render() {
-        const {soloManual} = this.props;
+    detenerAutomatico = () => {
+        this.setState({
+            auto: false, // paso de auto a manual
+            numeroPrimoM: this.state.numeroPrimoA // si en automatico se encontraba en el numero primo 5 -> en manual debe continuar desde 5
+        })
+    };
 
-        return(
-            <>
-                <Primo soloManual={soloManual} numeroPrimo={soloManual ? (this.state.numeroPrimo): (this.state.numeroPrimoAuto)}
-                        siguientePrimo={this.siguientePrimo} anteriorPrimo={this.anteriorPrimo}/>
-            </>
+    continuarAutomatico = () => {
+        this.setState({
+            auto: true, // paso de manual a auto
+            numeroPrimoA: this.state.numeroPrimoM // si en manual se encontraba en el numero primo 5 -> en automatico debe continuar desde 5
+        })
+    };
+
+    render() {
+        const { soloManual } = this.props; // soloManual: solo es manual - pregunta 2.a
+
+        return (
+            <div>
+                <Primo manual={soloManual || !this.state.auto}
+                    numeroPrimo={soloManual ? (this.state.numeroPrimoM) : (!this.state.auto ? this.state.numeroPrimoM : this.state.numeroPrimoA)}
+                    siguientePrimo={this.siguientePrimo} anteriorPrimo={this.anteriorPrimo} />
+                {
+                    !soloManual && ( // si no es solo manual o sea es el caso de las 2 funciones: manual y auto
+                        <>
+                            {
+                                this.state.auto ? ( // Cuando hay las 2 fn de manual y auto: eligio automatico
+                                    <div className="d-flex justify-content-center mt-2">
+                                        <button className="btn btn-primary me-2" onClick={this.detenerAutomatico}>Parar</button>
+                                    </div>
+                                ) : ( // Cuando hay las 2 fn de manual y auto: eligio manual
+                                    <div className="d-flex justify-content-center mt-2">
+                                        <button className="btn btn-warning" onClick={this.continuarAutomatico}>Continuar Automático</button>
+                                    </div>
+                                )
+                            }
+                        </>
+                    )
+                }
+            </div>
         );
     }
 }
