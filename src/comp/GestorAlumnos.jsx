2e68348dd8af46f9e8ea38d03b361dd99e4dba97
marisolkarina <marisol.kari.20@gmail.com>
2025-07-06 12:36:52 -0500

    mejora de estado de alumno

--- src/comp/GestorAlumnos.jsx
+++ src/comp/GestorAlumnos.jsx
@@ -10,8 +10,14 @@ const GestorAlumnos = () => {
         nombres: '',
         dni: '',
         celular: '',
+        estado: 1,
         id: null
-    })
+    });
+    // estado:
+    // 0 = eliminado
+    // 1 = activo
+    // 2 = retirado
+    // 3 = suspendido
 
     useEffect(() => {
         obtenerAlumnos();
@@ -33,7 +39,6 @@ const GestorAlumnos = () => {
                 dni: form.dni,
                 celular: form.celular,
                 estado: 1
-
             });
 
             obtenerAlumnos();
@@ -49,6 +54,7 @@ const GestorAlumnos = () => {
                 nombres: '',
                 dni: '',
                 celular: '',
+                estado: 1,
                 id: null
             }
         )
@@ -61,7 +67,7 @@ const GestorAlumnos = () => {
                 nombres: form.nombres,
                 dni: form.dni,
                 celular: form.celular,
-                estado: 1
+                estado: form.estado
             });
             obtenerAlumnos();
             limpiaFormulario();
@@ -76,8 +82,9 @@ const GestorAlumnos = () => {
     }
 
     const manejarCambio = (e) => {
+        const { name, value } = e.target;
         setForm(
-            { ...form, [e.target.name]: e.target.value }
+            { ...form, [name]: name === "estado" ? parseInt(value) : value }
         )
     }
 
@@ -135,13 +142,22 @@ const GestorAlumnos = () => {
                             placeholder="dni" className="form-control my-2" />
                         <input type="text" name="celular" required value={form.celular} onChange={manejarCambio}
                             placeholder="celular" className="form-control my-2" />
+                        {
+                            modoEdicion ? (
+                                <select name="estado" value={form.estado} onChange={manejarCambio} className="form-control my-2">
+                                    <option value="1">Activo</option>
+                                    <option value="2">Retirado</option>
+                                    <option value="3">Suspendido</option>
+                                </select>
+                            ) : (<></>)
+                        }
 
                         <div className="d-flex justify-content-between mt-3">
                             <input type="submit" className="btn btn-success" value={modoEdicion ? 'Actualizar' : 'Registrar'} />
                             {
                                 (modoEdicion && (
-                                    <input type="submit" className="btn btn-danger"
-                                        onClick={limpiaFormulario} value="Cancelar" />
+                                    <input type="button" className="btn btn-danger"
+                                        onClick={limpiaFormulario} value="Cancelar"/>
                                 ))
                             }
                         </div>
@@ -162,10 +178,14 @@ const GestorAlumnos = () => {
                                             <p><strong>ID:</strong> {a.id}</p>
                                             <p><strong>DNI:</strong> {a.dni}</p>
                                             <p><strong>Celular:</strong> {a.celular}</p>
-                                            <p><strong>Estado:</strong> { a.estado===0 ? '0-> inactivo' : '1-> activo'}</p>
+                                            <p><strong>Estado:</strong> {
+                                                a.estado === 0 ? 'Eliminado' :
+                                                    a.estado === 1 ? 'Activo' :
+                                                        a.estado === 2 ? 'Retirado' : 'Suspendido'
+                                            }</p>
                                         </div>
                                         {
-                                            a.estado === 1 ? ( // solo si es activo se puede editar/eliminar
+                                            a.estado !== 0 ? ( // solo si es activo se puede editar/eliminar
                                                 <div className="card-footer d-flex justify-content-between">
                                                     <button className="btn btn-warning" onClick={() => editarAlumno(a)}>Editar</button>
                                                     <button className="btn btn-danger" onClick={() => eliminarAlumno(a)}>Eliminar</button>
@@ -181,38 +201,49 @@ const GestorAlumnos = () => {
                 </div>
             </div>
             <div className="row mt-5">
-                <p className="fs-3 text-center text-danger">Alumnos eliminados</p>
-                <div className="table-responsive">
-                    <table className="table table-striped table-bordered table-hover text-center mx-auto" style={{ width: '90%' }}>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nombres</th>
-                                <th>DNI</th>
-                                <th>Celular</th>
-                                <th>Estado</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                alumnosEliminados.map((a) => (
-                                    <tr key={a.id}>
-                                        <td>{a.id}</td>
-                                        <td>{a.nombres}</td>
-                                        <td>{a.dni}</td>
-                                        <td>{a.celular}</td>
-                                        <td>{a.estado}</td>
-                                        <td>
-                                            <button className="btn btn-success mx-2"
-                                                onClick={() => restaurar(a)}>Restaurar</button>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                {
+                    alumnosEliminados.length > 0 ? (
+                        <>
+                            <p className="fs-3 text-center text-danger">Alumnos eliminados</p>
+                            <div className="table-responsive">
+                                <table className="table table-striped table-bordered table-hover text-center mx-auto" style={{ width: '90%' }}>
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nombres</th>
+                                            <th>DNI</th>
+                                            <th>Celular</th>
+                                            <th>Estado</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            alumnosEliminados.map((a) => (
+                                                <tr key={a.id}>
+                                                    <td>{a.id}</td>
+                                                    <td>{a.nombres}</td>
+                                                    <td>{a.dni}</td>
+                                                    <td>{a.celular}</td>
+                                                    <td>
+                                                        {a.estado} : Eliminado
+                                                    </td>
+                                                    <td>
+                                                        <button className="btn btn-success mx-2"
+                                                            onClick={() => restaurar(a)}>Restaurar</button>
+                                                    </td>
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </>
+                    ) : (
+                        <p className="fs-3 text-center text-danger">No hay alumnos eliminados</p>
+                    )
+                }
+
             </div>
         </main>
     );
